3d0e93bfbbdc4a97e41e79350d8e8c1ec44553c7
cometj03 <harryjung1001@gmail.com>
2022-06-24 15:58:25 +0900

    1932

---
+++ 16.dp1/1932.py
@@ -0,0 +1,16 @@
+# 정수 삼각형
+n = int(input())
+arr = []
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    arr.append(nums)
+
+for i in range(1, n):
+    arr[i][0] += arr[i-1][0]
+    arr[i][-1] += arr[i-1][-1]
+    for j in range(1, i):
+        arr[i][j] += max(arr[i-1][j], arr[i-1][j-1])
+
+print(max(arr[n-1]))
+print(arr[n-1])
