545880ce4c943771a62d292e1da5a873e5b3bcd4
cometj03 <harryjung1001@gmail.com>
2022-06-27 21:43:17 +0900

    2579

--- 16.dp1/2579.py
+++ 16.dp1/2579.py
@@ -1,5 +1,12 @@
+# 계단 오르기
 n = int(input())
-scores = [int(input()) for _ in range(n)]
+scores = [int(input()) for _ in range(n)] + [0, 0] # n이 3미만일 때 대비
+dp = [0] * (n+3)
 
-for i in range(2, n):
-    pass
+dp[0] = scores[0]
+dp[1] = scores[0] + scores[1]
+dp[2] = max(scores[0], scores[1]) + scores[2]
+for i in range(3, n):
+    dp[i] = max(dp[i-3] + scores[i-1], dp[i-2]) + scores[i]
+
+print(dp[n-1])
