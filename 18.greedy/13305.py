ba08661cb42dd48e2cea4aa265121b7c7b56bb02
cometj03 <harryjung1001@gmail.com>
2022-07-19 23:47:27 +0900

    13305 주유소

---
+++ 18.greedy/13305.py
@@ -0,0 +1,17 @@
+# 주유소
+N = int(input())
+road = [*map(int, input().split())]
+price = [*map(int, input().split())]
+cost = 0
+
+i = 0
+while i < N - 1:
+    length = road[i]
+    k = i + 1
+    while price[k] > price[i]:
+        length += road[k]
+        k += 1
+    cost += length * price[i]
+    i = k
+
+print(cost)
