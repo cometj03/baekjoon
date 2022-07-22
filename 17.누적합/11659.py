5a92eaf30dc031770e22471bbdba4968b623fc6f
cometj03 <harryjung1001@gmail.com>
2022-07-22 14:44:11 +0900

    11659 구간 합 구하기 4

--- 17.누적합/11659.py
+++ 17.누적합/11659.py
@@ -1,13 +1,14 @@
-# 구간 합 구하기
-
-# TODO: 시간초과
+# 구간 합 구하기 4
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = list(map(int, input().split()))
+arr = [0] + [*map(int, input().split())]
+
+for i in range(2, len(arr)):
+    arr[i] += arr[i-1]
 
 while M:
     M -= 1
-    i, j = map(int, input().split())
-    print(sum(arr[i-1:j]))
+    a, b = map(int, input().split())
+    print(arr[b] - arr[a-1])
